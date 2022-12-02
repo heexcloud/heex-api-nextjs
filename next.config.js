ab5682e9c8d3b06f9a33abdf8270bb21cb6546bc
Yumin Gui <yumindev@gmail.com>
2022-12-01 18:50:58 -0800

    fix: remove dotenv since nextjs supports dotenv out of box

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-const fs = require("fs");
-const filePath = "./.env";
-if (process.env.NODE_ENV === "development") {
-  if (fs.existsSync(filePath)) {
-    require("dotenv").config();
-  } else {
-    throw new Error("Couldn't find .env file");
-  }
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
