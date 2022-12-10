793a717848c51e079cb4ce7e5394365efe217996
Yumin Gui <yumindev@gmail.com>
2022-12-10 09:30:18 -0800

    fix: better api

--- pages/api/v1/comment/count.ts
+++ pages/api/v1/comment/count.ts
@@ -4,29 +4,30 @@ import { query } from "root/query";
 import { RESPONSE_CODE } from "root/utils";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    if (!req.query.pageId || typeof req.query.pageId !== "string") {
-      return res.status(200).json({
-        data: null,
-        code: RESPONSE_CODE.BAD_REQUEST_PARAM_MISSING,
-        message: "pageId is required",
-      });
-    }
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
+    res.status(425).end(`Method ${req.method} is not allowed.`);
+    return;
+  }
 
-    const result = await query.databaseProvider.getCommentCount({
-      pageId: req.query.pageId as string,
-      clientId: req.query.clientId as string,
-    });
-
+  if (!req.query.pageId || typeof req.query.pageId !== "string") {
     return res.status(200).json({
-      data: result,
-      code: RESPONSE_CODE.GENERAL_SUCCESS,
-      message: "comment count is " + result.count,
+      data: null,
+      code: RESPONSE_CODE.BAD_REQUEST_PARAM_MISSING,
+      message: "pageId is required",
     });
   }
 
-  res.setHeader("Allow", ["GET"]);
-  res.status(425).end(`Method ${req.method} is not allowed.`);
+  const result = await query.databaseProvider.getCommentCount({
+    pageId: req.query.pageId as string,
+    clientId: req.query.clientId as string,
+  });
+
+  return res.status(200).json({
+    data: result,
+    code: RESPONSE_CODE.GENERAL_SUCCESS,
+    message: "comment count is " + result.count,
+  });
 }
 
 export default middlewares.cors(handler);
